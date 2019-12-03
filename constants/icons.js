395fc54674eebbdfccdc8b1c52492942658eff9e
unknown <pmurph0305@gmail.com>
2019-12-03 11:08:31 -0500

    Move data to constants, allIcons which are untagged icons for user to use in custom tags

---
+++ constants/icons.js
@@ -0,0 +1,78 @@
+export const fontAwesomeIcons = [
+  { iconName: "shopping-cart", displayName: "shopping" },
+  { iconName: "book", displayName: "reading" },
+  { iconName: "heart", displayName: "date" },
+  { iconName: "briefcase", displayName: "work" },
+  { iconName: "plane", displayName: "travel" },
+  { iconName: "users", displayName: "friends" },
+  { iconName: "gamepad", displayName: "gaming" }
+];
+
+export const featherIcons = [
+  { iconName: "tv", displayName: "television" },
+  { iconName: "monitor", displayName: "computer" },
+  { iconName: "moon", displayName: "good sleep" }
+];
+export const materialIcons = [
+  { iconName: "beach-access", displayName: "relax" },
+  { iconName: "phone", displayName: "phone" },
+  { iconName: "directions-run", displayName: "exercise" },
+  { iconName: "restaurant", displayName: "good food" }
+];
+
+export const allIcons = {
+  fontAwesomeIcons: [
+    { iconName: "shopping-cart" },
+    { iconName: "book" },
+    { iconName: "heart" },
+    { iconName: "briefcase" },
+    { iconName: "plane" },
+    { iconName: "users" },
+    { iconName: "gamepad" },
+    { iconName: "music" },
+    { iconName: "camera" },
+    { iconName: "gift" },
+    { iconName: "wrench" },
+    { iconName: "desktop" },
+    { iconName: "laptop" },
+    { iconName: "tablet" },
+    { iconName: "mobile" },
+    { iconName: "rocket" },
+    { iconName: "bank" },
+    { iconName: "paw" },
+    { iconName: "soccer-ball-o" },
+    { iconName: "paint-brush" },
+    { iconName: "pie-chart" },
+    { iconName: "bicycle" },
+    { iconName: "subway" },
+    { iconName: "thermometer" },
+    { iconName: "snowflake-o" }
+  ],
+  featherIcons: [
+    { iconName: "tv" },
+    { iconName: "monitor" },
+    { iconName: "moon" },
+    { iconName: "activity" },
+    { iconName: "archive" },
+    { iconName: "cloud-drizzle" },
+    { iconName: "headphones" },
+    { iconName: "sun" }
+  ],
+  materialIcons: [
+    { iconName: "beach-access" },
+    { iconName: "phone" },
+    { iconName: "directions-run" },
+    { iconName: "restaurant" },
+    { iconName: "drive-eta" },
+    { iconName: "grade" },
+    { iconName: "gold-course" },
+    { iconName: "local-hospital" },
+    { iconName: "hotel" },
+    { iconName: "kitchen" },
+    { iconName: "local-cafe" },
+    { iconName: "local-florist" },
+    { iconName: "location-city" },
+    { iconName: "nature" },
+    { iconName: "pool" }
+  ]
+};
