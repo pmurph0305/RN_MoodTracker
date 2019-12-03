fcd2558eaed8d03fec88ff7da9ed680d71d823fb
unknown <pmurph0305@gmail.com>
2019-12-03 11:09:33 -0500

    Remove data from database.js, import from constants

--- database/database.js
+++ database/database.js
@@ -1,144 +1,17 @@
 import * as SQLite from "expo-sqlite";
 
-const moodData = [
-  {
-    date: "2019-12-02T14:48:00.000Z",
-    tags: [8, 1, 2],
-    rating: 45.5,
-    note: "good day"
-  },
-  {
-    date: "2019-11-26T12:00:00.000Z",
-    tags: [3, 12, 5],
-    rating: 55
-  },
-  {
-    date: "2019-11-23T09:43:00.000Z",
-    tags: [1, 4, 3],
-    rating: 75
-  },
-  {
-    date: "2019-11-01T01:02:00.000Z",
-    tags: [3, 13, 8, 9],
-    rating: 21,
-    note: "bad day"
-  },
-  {
-    date: "2019-11-28T23:59:59.000Z",
-    tags: [2, 5, 8, 4],
-    rating: 43
-  },
-  {
-    date: "2019-11-28T23:58:50.000Z",
-    tags: [5, 14, 3, 1, 2, 4, 6, 7],
-    rating: 65
-  },
-  {
-    date: "2019-11-29T00:01:00.000Z",
-    tags: [11, 2, 10],
-    rating: 75
-  }
-];
+import { moodData } from "../constants/mooddata";
+import {
+  fontAwesomeIcons,
+  featherIcons,
+  materialIcons
+} from "../constants/icons";
 
-const fontAwesomeIcons = [
-  { iconName: "shopping-cart", displayName: "shopping" },
-  { iconName: "book", displayName: "reading" },
-  { iconName: "heart", displayName: "date" },
-  { iconName: "briefcase", displayName: "work" },
-  { iconName: "plane", displayName: "travel" },
-  { iconName: "users", displayName: "friends" },
-  { iconName: "gamepad", displayName: "gaming" }
-];
-
-const featherIcons = [
-  { iconName: "tv", displayName: "television" },
-  { iconName: "monitor", displayName: "computer" },
-  { iconName: "moon", displayName: "good sleep" }
-];
-const materialIcons = [
-  { iconName: "beach-access", displayName: "relax" },
-  { iconName: "phone", displayName: "phone" },
-  { iconName: "directions-run", displayName: "exercise" },
-  { iconName: "restaurant", displayName: "good food" }
-];
 export default class Database {
   static db = SQLite.openDatabase("MoodDB.db", "0", "Mood Database");
   constructor() {
     this.db = SQLite.openDatabase("MoodDB.db", "0", "Mood Database");
   }
-
-  insertTag = (iconType, iconData) => {
-    return this.executeSql(
-      "INSERT OR IGNORE INTO tags(iconType, iconName, displayName) VALUES (?, ?, ?);",
-      [iconType, iconData.iconName, iconData.displayName]
-    );
-  };
-
-  seedTags = () => {
-    console.log("Seeding tags...");
-    let promises = [];
-    fontAwesomeIcons.forEach(icon => {
-      promises.push(this.insertTag("FontAwesome", icon));
-    });
-    materialIcons.forEach(icon => {
-      promises.push(this.insertTag("MaterialIcons", icon));
-    });
-    featherIcons.forEach(icon => {
-      promises.push(this.insertTag("Feather", icon));
-    });
-    return Promise.all(promises)
-      .then(() => {
-        return Promise.resolve("All reinserting of tags completed");
-      })
-      .catch(error => {
-        console.log("Error with all seed promises", error);
-      });
-  };
-
-  seedMoods = () => {
-    console.log("Seeding moods...");
-    let promises = [];
-    moodData.forEach(mood => {
-      promises.push(this.insertMood(mood));
-    });
-    return Promise.all(promises).then(() => {
-      return Promise.resolve("Succesfully reseeded moods");
-    });
-  };
-
-  insertMood = mood => {
-    return this.executeFullSql(
-      "INSERT INTO moods (rating, date, note) VALUES (?, ?, ?);",
-      [mood.rating, mood.date, mood.note]
-    )
-      .then(result => {
-        let promises = [];
-        mood.tags.forEach(tag => {
-          promises.push(
-            this.executeSql(
-              "INSERT INTO tagmap (moodId, tagId) VALUES (?, ?);",
-              [result.insertId, tag]
-            )
-          );
-        });
-        return Promise.all(promises).then(() => {
-          return Promise.resolve(
-            "Successfully inserted mood " + result.insertId
-          );
-        });
-      })
-      .catch(error => {
-        console.log("ERROR", error);
-        reject(error);
-      });
-  };
-
-  insertTagMap = (moodId, tagId) => {
-    return this.executeFullSql(
-      "INSERT INTO tagmap (moodId, tagId) VALUES(?,?);",
-      [moodId, tagId]
-    );
-  };
 
   deleteAllTables = () => {
     return this.executeSql("drop table if exists moods;").then(() => {
@@ -183,6 +56,38 @@ export default class Database {
       });
   };
 
+  seedTags = () => {
+    console.log("Seeding tags...");
+    let promises = [];
+    fontAwesomeIcons.forEach(icon => {
+      promises.push(this.insertTag("FontAwesome", icon));
+    });
+    materialIcons.forEach(icon => {
+      promises.push(this.insertTag("MaterialIcons", icon));
+    });
+    featherIcons.forEach(icon => {
+      promises.push(this.insertTag("Feather", icon));
+    });
+    return Promise.all(promises)
+      .then(() => {
+        return Promise.resolve("All reinserting of tags completed");
+      })
+      .catch(error => {
+        console.log("Error with all seed promises", error);
+      });
+  };
+
+  seedMoods = () => {
+    console.log("Seeding moods...");
+    let promises = [];
+    moodData.forEach(mood => {
+      promises.push(this.insertMood(mood));
+    });
+    return Promise.all(promises).then(() => {
+      return Promise.resolve("Succesfully reseeded moods");
+    });
+  };
+
   executeFullSql = (sql, params = []) => {
     return new Promise((resolve, reject) => {
       this.db.transaction(tx => {
@@ -210,6 +115,47 @@ export default class Database {
           error => reject(error)
         );
       })
+    );
+  };
+
+  insertMood = mood => {
+    return this.executeFullSql(
+      "INSERT INTO moods (rating, date, note) VALUES (?, ?, ?);",
+      [mood.rating, mood.date, mood.note]
+    )
+      .then(result => {
+        let promises = [];
+        mood.tags.forEach(tag => {
+          promises.push(
+            this.executeSql(
+              "INSERT INTO tagmap (moodId, tagId) VALUES (?, ?);",
+              [result.insertId, tag]
+            )
+          );
+        });
+        return Promise.all(promises).then(() => {
+          return Promise.resolve(
+            "Successfully inserted mood " + result.insertId
+          );
+        });
+      })
+      .catch(error => {
+        console.log("ERROR", error);
+        reject(error);
+      });
+  };
+
+  insertTag = (iconType, iconData) => {
+    return this.executeSql(
+      "INSERT OR IGNORE INTO tags(iconType, iconName, displayName) VALUES (?, ?, ?);",
+      [iconType, iconData.iconName, iconData.displayName]
+    );
+  };
+
+  insertTagMap = (moodId, tagId) => {
+    return this.executeFullSql(
+      "INSERT INTO tagmap (moodId, tagId) VALUES(?,?);",
+      [moodId, tagId]
     );
   };
 
